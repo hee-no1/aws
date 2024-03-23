8bac0e0a2e741415b74ce13e6a386ab0ca13b442
hee-no1 <lhw817@naver.com>
2024-03-23 19:58:03 +0900

    second commit

--- src/main/java/aws/controller/MainController.java
+++ src/main/java/aws/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
     @GetMapping("/")
-    public String main(){
+    public String home(){
         return "메인페이지!!!";
     }
 }
